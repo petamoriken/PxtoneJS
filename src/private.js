b0524b7312c86e9e6f210f0317011e1a14eb822b
petamoriken <suteaka_sayonara@yahoo.co.jp>
2016-02-13 04:35:41 +0900

    optimize src/private.js

--- src/private.js
+++ src/private.js
@@ -1,4 +1,13 @@
 export function createPrivateStorage() {
 	const wm = new WeakMap();
-	return self => wm.get(self) || wm.set(self, Object.create(null)).get(self);
+	return self => {
+		let obj = wm.get(self);
+		if(obj) {
+			return obj;
+		} else {
+			obj = Object.create(null);
+			wm.set(self, obj);
+			return obj;
+		}
+	}
 }
